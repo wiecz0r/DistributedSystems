b8f28417cf4d4571f972e984c8b425ff6923ea23
wiecz0r <wieczor1997@gmail.com>
2019-04-12 14:10:48 +0200

    Added scheme

--- 3_RabbitMQ/src/main/java/src/pl/edu/agh/student/swieczor/rabbitmqhospital/test.java
+++
@@ -1,7 +0,0 @@
-package src.pl.edu.agh.student.swieczor.rabbitmqhospital;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println(String.join(".","info"));
-    }
-}
